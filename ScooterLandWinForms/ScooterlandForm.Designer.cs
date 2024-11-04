471ad9911feaa6f0414c8ac1f240f9c919178f39
Philip Holm Møller <phmo61881@edu.ucl.dk>
2024-11-04 22:46:17 +0100

    ui change in forms

--- ScooterLandWinForms/ScooterlandForm.Designer.cs
+++ ScooterLandWinForms/ScooterlandForm.Designer.cs
@@ -37,12 +37,19 @@
             buttonBackup = new Button();
             buttonSelectFolder = new Button();
             buttonBackupLocation = new Button();
+            groupBox1 = new GroupBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // labelPath
             // 
             labelPath.AutoSize = true;
-            labelPath.Location = new Point(165, 108);
+            labelPath.Location = new Point(133, 108);
             labelPath.Name = "labelPath";
             labelPath.Size = new Size(170, 20);
             labelPath.TabIndex = 0;
@@ -50,10 +57,10 @@
             // 
             // textBoxFrom
             // 
-            textBoxFrom.Location = new Point(237, 145);
+            textBoxFrom.Location = new Point(205, 145);
             textBoxFrom.Name = "textBoxFrom";
             textBoxFrom.ReadOnly = true;
-            textBoxFrom.Size = new Size(391, 27);
+            textBoxFrom.Size = new Size(319, 27);
             textBoxFrom.TabIndex = 1;
             // 
             // label1
@@ -68,15 +75,15 @@
             // 
             // textBoxUSB
             // 
-            textBoxUSB.Location = new Point(237, 250);
+            textBoxUSB.Location = new Point(205, 250);
             textBoxUSB.Name = "textBoxUSB";
-            textBoxUSB.Size = new Size(391, 27);
+            textBoxUSB.Size = new Size(319, 27);
             textBoxUSB.TabIndex = 3;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(175, 214);
+            label2.Location = new Point(143, 214);
             label2.Name = "label2";
             label2.Size = new Size(129, 20);
             label2.TabIndex = 4;
@@ -95,7 +102,7 @@
             // 
             // buttonSelectFolder
             // 
-            buttonSelectFolder.Location = new Point(141, 144);
+            buttonSelectFolder.Location = new Point(109, 144);
             buttonSelectFolder.Name = "buttonSelectFolder";
             buttonSelectFolder.Size = new Size(94, 29);
             buttonSelectFolder.TabIndex = 6;
@@ -105,7 +112,7 @@
             // 
             // buttonBackupLocation
             // 
-            buttonBackupLocation.Location = new Point(140, 249);
+            buttonBackupLocation.Location = new Point(108, 249);
             buttonBackupLocation.Name = "buttonBackupLocation";
             buttonBackupLocation.Size = new Size(94, 29);
             buttonBackupLocation.TabIndex = 7;
@@ -113,11 +120,71 @@
             buttonBackupLocation.UseVisualStyleBackColor = true;
             buttonBackupLocation.Click += buttonBackupLocation_Click;
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Location = new Point(552, 108);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(212, 214);
+            groupBox1.TabIndex = 8;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Guide";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 40);
+            label3.Name = "label3";
+            label3.Size = new Size(199, 20);
+            label3.TabIndex = 0;
+            label3.Text = "1. Vælg mappe som der skal ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(20, 65);
+            label4.Name = "label4";
+            label4.Size = new Size(137, 20);
+            label4.TabIndex = 1;
+            label4.Text = "laves en back-up af";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(7, 109);
+            label5.Name = "label5";
+            label5.Size = new Size(198, 20);
+            label5.TabIndex = 2;
+            label5.Text = "2. Vælg placering af back-up";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(21, 130);
+            label6.Name = "label6";
+            label6.Size = new Size(168, 20);
+            label6.TabIndex = 3;
+            label6.Text = "på det indsatte USB-stik";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(7, 167);
+            label7.Name = "label7";
+            label7.Size = new Size(190, 20);
+            label7.TabIndex = 4;
+            label7.Text = "3. Tryk på back-up knappen";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
             Controls.Add(buttonBackupLocation);
             Controls.Add(buttonSelectFolder);
             Controls.Add(buttonBackup);
@@ -128,6 +195,8 @@
             Controls.Add(labelPath);
             Name = "Form1";
             Text = "ScooterLand ";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -143,5 +212,11 @@
         private Button buttonBackup;
         private Button buttonSelectFolder;
         private Button buttonBackupLocation;
+        private GroupBox groupBox1;
+        private Label label4;
+        private Label label3;
+        private Label label6;
+        private Label label5;
+        private Label label7;
     }
 }
