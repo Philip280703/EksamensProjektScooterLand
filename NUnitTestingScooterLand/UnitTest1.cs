ddf6106fe6eafab7cdd7eae20cf7ad942cd4653b
Philip Holm Møller <phmo61881@edu.ucl.dk>
2024-11-11 10:11:23 +0100

    added unittest projekt

---
+++ NUnitTestingScooterLand/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NUnitTestingScooterLand
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
