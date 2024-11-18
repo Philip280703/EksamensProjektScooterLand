394d864d9ace073adbfc528eaf166c3358ffe6ab
MarkKochRuge <165993382+MarkKochRuge@users.noreply.github.com>
2024-11-18 13:50:55 +0100

    Forbedret Kunde Pages

--- EksamensProjektScooterLandBlazor/Client/Pages/KundePage.razor.cs
+++ EksamensProjektScooterLandBlazor/Client/Pages/KundePage.razor.cs
@@ -1,7 +1,19 @@
-﻿namespace EksamensProjektScooterLandBlazor.Client.Pages
+﻿using EksamensProjektScooterLandBlazor.Shared.Models;
+using EksamensProjektScooterLandBlazor.Client.Services;
+using Microsoft.AspNetCore.Components;
+namespace EksamensProjektScooterLandBlazor.Client.Pages
 {
 	public partial class KundePage
 	{
+		private List<Kunde> kundeListe = new List<Kunde>();
+		public int ErrorCode { get; set; }
 
+		[Inject]
+		public IKundeService Service { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			kundeListe = (await Service.GetAllKunder()).ToList();
+		}
 	}
 }
