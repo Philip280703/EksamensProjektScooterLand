ccb13ed3cdf9c3987618aa00a1920dafb5ebabfb
Philip Holm Møller <phmo61881@edu.ucl.dk>
2024-11-18 11:11:43 +0100

    trying to display mekaniker

--- EksamensProjektScooterLandBlazor/Client/Program.cs
+++ EksamensProjektScooterLandBlazor/Client/Program.cs
@@ -1,11 +1,19 @@
 using EksamensProjektScooterLandBlazor.Client;
+using EksamensProjektScooterLandBlazor.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Register MekanikerService
+builder.Services.AddHttpClient<IMekanikerService, MekanikerService>(client =>
+{
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+});
 
 await builder.Build().RunAsync();
---
+++ EksamensProjektScooterLandBlazor/Client/Services/IMekanikerService.cs
@@ -0,0 +1,11 @@
+﻿using EksamensProjektScooterLandBlazor.Shared.Models;
+
+namespace EksamensProjektScooterLandBlazor.Client.Services
+{
+    public interface IMekanikerService
+    {
+        Task<Mekaniker[]?> GetAllMekaniker();
+        Task<Mekaniker> GetMekaniker(int id);
+
+    }
+}
