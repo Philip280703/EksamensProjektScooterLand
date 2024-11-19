db93e5e3472b665ae9e8f9d25ac739ec433e218d
Andreas Fiil Thomsen <afth61680@edu.ucl.dk>
2024-11-19 09:36:22 +0100

    Fixing bug gg.

--- EksamensProjektScooterLandBlazor/Client/Program.cs
+++ EksamensProjektScooterLandBlazor/Client/Program.cs
@@ -1,4 +1,5 @@
 using EksamensProjektScooterLandBlazor.Client;
+using EksamensProjektScooterLandBlazor.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// måske unødvendig
+builder.Services.AddScoped<IYdelseService, YdelseService>();
+
 
 await builder.Build().RunAsync();
---
+++ EksamensProjektScooterLandBlazor/Server/Repositories/IYdelseRepository.cs
@@ -0,0 +1,17 @@
+﻿using EksamensProjektScooterLandBlazor.Shared.Models;
+using EksamensProjektScooterLandBlazor.Client.Interfaces;
+
+
+namespace EksamensProjektScooterLandBlazor.Server.Repositories
+{
+    public interface IYdelseRepository
+    {
+        List<Ydelse> GetAllYdelser();
+        Ydelse GetYdelse(int id);
+        
+        void AddYdelse(Ydelse ydelse);
+        bool DeleteYdelse(int id);
+        bool UpdateYdelse(Ydelse ydelse);
+
+    }
+}
