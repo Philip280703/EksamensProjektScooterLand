506e52ff9ba3366d430beaa2620934ef5bd633b1
MarkKochRuge <165993382+MarkKochRuge@users.noreply.github.com>
2024-11-26 09:57:15 +0100

    Backup project

---
+++ EksamensProjektScooterLandBlazor/Server/Controllers/WinFormsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EksamensProjektScooterLandBlazor.Server.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class WinFormsController : ControllerBase
+	{
+		[HttpGet("åbnbackupform")]
+		public IActionResult ÅbnBackupForm()
+		{
+            Console.WriteLine("Åbnbackupform kaldet");
+			try
+			{
+				string path = "EksamensProjektScooterLand\\ScooterLandWinForms\\bin\\Debug\\net8.0-windows\\ScooterLandWinForms.exe";
+				Process.Start("C:\\Users\\markr\\source\\repos\\"+path);
+				return Ok("Backup Projekt startet");
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"Fejl: {ex.Message}");
+			}
+		}
+	}
+}
