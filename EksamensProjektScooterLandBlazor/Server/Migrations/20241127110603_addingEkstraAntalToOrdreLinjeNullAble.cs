0989e8db97e3eb6b21304ba43eaad11e8e300632
Philip Holm Møller <phmo61881@edu.ucl.dk>
2024-11-27 14:39:29 +0100

    adding scooterleje til ordrelinje

---
+++ EksamensProjektScooterLandBlazor/Server/Migrations/20241127110603_addingEkstraAntalToOrdreLinjeNullAble.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EksamensProjektScooterLandBlazor.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingEkstraAntalToOrdreLinjeNullAble : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AntalEkstra",
+                table: "OrdreLinjer",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AntalEkstra",
+                table: "OrdreLinjer");
+        }
+    }
+}
