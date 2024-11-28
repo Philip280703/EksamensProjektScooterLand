cfb624207f8d2024de9dffa1e1f7a2e6fc8622bb
Benjamin <beha61676@edu.ucl.dk>
2024-11-28 10:12:10 +0100

    styling af produktpage

---
+++ EksamensProjektScooterLandBlazor/Client/ChildComponents/RenderProdukt.razor.cs
@@ -0,0 +1,66 @@
+﻿using EksamensProjektScooterLandBlazor.Client.Services;
+using EksamensProjektScooterLandBlazor.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
+
+namespace EksamensProjektScooterLandBlazor.Client.ChildComponents
+{
+	public partial class RenderProdukt
+	{
+		[Parameter]
+		public Produkt produkt { get; set; }
+
+		private OrdreLinje ordreLinje = new OrdreLinje();
+
+		//bliver brugt nede i Deleteprodukt metoden men den bliver ikke brugt lige nu da det ikke skal kunnes
+		//implementeres at man kan slette et produkt
+		[Parameter]
+		public EventCallback<Produkt> deleteProdukt { get; set; }
+
+		[Parameter]
+		public int OrdreId { get; set; }
+
+		[Parameter]
+		public List<OrdreLinje> ordreLinjeList { get; set; }
+
+		[Inject]
+		public IProduktService Service { get; set; }
+
+		[Inject]
+		public IOrdreLinjeService ordreLinjeService { get; set; }
+
+		[Parameter]
+		public bool OrdreAddBool { get; set; } = false;
+
+		[Parameter]
+		public EventCallback Produkttilføjet { get; set; }
+
+		private int ErrorCode { get; set; } = 0;
+
+		private async Task DeleteProdukt()
+		{
+			// await deleteProdukt.InvokeAsync(produkt);
+			var placeholder = "intet at se her...";
+			Console.WriteLine(placeholder);
+
+		}
+
+		private async Task AddProductToOrdre()
+		{
+			ordreLinje.ProduktID = produkt.ProduktID;
+			ordreLinje.Antal = 1;
+			ordreLinje.OrdreLinjeDato = DateTime.Now;
+			ordreLinje.RabatProcent = 0;
+			ordreLinje.OrdreID = OrdreId;
+			ErrorCode = await ordreLinjeService.AddOrdreLinje(ordreLinje);
+
+			await Produkttilføjet.InvokeAsync();
+		}
+
+
+		private void NavigateToEditproduktComponent(int produktID)
+		{
+			NavigationsManager.NavigateTo($"/Editproduktpage/{produktID}");
+		}
+	}
+}
