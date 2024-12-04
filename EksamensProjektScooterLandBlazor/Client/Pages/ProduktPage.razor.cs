d0b6f79add43377fe6a46cc056a52d8855f3079a
MarkKochRuge <165993382+MarkKochRuge@users.noreply.github.com>
2024-12-04 11:04:32 +0100

    UI design fixet. + fixet submenuer og navlinks

--- EksamensProjektScooterLandBlazor/Client/Pages/ProduktPage.razor.cs
+++ EksamensProjektScooterLandBlazor/Client/Pages/ProduktPage.razor.cs
@@ -15,7 +15,9 @@ namespace EksamensProjektScooterLandBlazor.Client.Pages
 		// Nyt item som oprettes gennem formen
 		private Produkt newProdukt = new Produkt();
 
-		private int ErrorCode { get; set; } = 0;
+		public bool visTilføjProdukt { get; set; } = false;
+
+		public int ErrorCode { get; set; } = 0;
 
 		private EditContext EditContext;
 
@@ -49,7 +51,10 @@ namespace EksamensProjektScooterLandBlazor.Client.Pages
 			// ErrorCode = await Service.DeleteProdukt(produkt.ProduktID);
 		}
 
-
+		public void visTilføjProduktSide()
+		{
+			visTilføjProdukt = !visTilføjProdukt;
+		}
 
 		protected override async Task OnInitializedAsync()
 		{
